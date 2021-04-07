31f28cb01d65fb2eebc1472222e6dd563952b69f
Julia Lozenko <jlozenko@gmail.com>
2021-04-07 14:55:23 +0300

    added project structure + homework 1 ready

---
+++ src/js/task-01.js
@@ -0,0 +1,16 @@
+
+
+// const ulEl = document.querySelectorAll('.item').length;
+// console.log(`В списке ${ulEl} категории.`);
+
+const ulEl = document.querySelector('ul');
+  
+const ulChild = ulEl.children.length;
+console.log(`В списке ${ulChild} категории.`);
+
+const liChildText = document.querySelectorAll('li.item').forEach(el => {
+  const category = el.querySelector('h2').textContent
+  const itemsLength = el.querySelector('ul').children.length
+  console.log(`- Категория: ${category}`);
+  console.log(`- Количество элементов: ${itemsLength}`);
+});
